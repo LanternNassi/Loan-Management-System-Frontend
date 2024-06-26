d3cc67c7ed2b7891a1764aa179418c50320296df
LanternNassi <Lanternnassi@gmail.com>
2024-06-26 19:36:15 +0300

    Add url

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 
 export const Custom_Axios = (token) => axios.create({
-    baseURL: 'https://localhost:7223/api',
+    // baseURL: 'https://localhost:7223/api',
+    baseURL: 'https://loan-backend-hskw.onrender.com/api',
     timeout: 10000,
     headers : {
         Authorization : "Bearer " + token 

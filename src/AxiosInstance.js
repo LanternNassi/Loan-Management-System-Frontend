9c80c9e52a5575af6cf3959094a76b87a12e46eb
LanternNassi <Lanternnassi@gmail.com>
2024-06-27 11:43:44 +0300

    Modifying env

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,10 +1,11 @@
 
 import axios from 'axios';
+import { BACKEND_URL } from '../config';
 
 
 export const Custom_Axios = (token) => axios.create({
     // baseURL: 'https://localhost:7223/api',
-    baseURL: 'https://loan-backend-hskw.onrender.com/api',
+    baseURL: BACKEND_URL,
     timeout: 10000,
     headers : {
         Authorization : "Bearer " + token 

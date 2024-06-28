5a5745e5fde117bfc930759612f665ebdb4de4af
LanternNassi <Lanternnassi@gmail.com>
2024-06-28 14:48:38 +0300

    Add url

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -7,7 +7,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 console.log(BACKEND_URL)
 export const Custom_Axios = (token) => axios.create({
     // baseURL: 'https://localhost:7223/api',
-    baseURL: BACKEND_URL,
+    baseURL: "https://loan-backend-hskw.onrender.com/api",
     timeout: 10000,
     headers : {
         Authorization : "Bearer " + token 

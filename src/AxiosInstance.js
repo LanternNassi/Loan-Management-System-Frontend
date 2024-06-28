87825c58a9874ae6c1614017f992e4424ed4430c
LanternNassi <Lanternnassi@gmail.com>
2024-06-28 13:43:51 +0300

    Prod

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-// console.log(BACKEND_URL)
+console.log(BACKEND_URL)
 export const Custom_Axios = (token) => axios.create({
     // baseURL: 'https://localhost:7223/api',
     baseURL: BACKEND_URL,
